1d9086abc764c0bb5568188311f56e1ae0c47146
muhammadasif46 <asifahmed32c@gmail.com>
2026-07-31 12:32:55 +0500

    javascript - chapter 4 to 8

---
+++ 03. JAVASCRIPT/02. class-two/app.js
@@ -0,0 +1,107 @@
+// var user$name = "Jhon"
+// var user_name = "Jhon"
+// var username = "Jhon"
+// var username1 = "Jhon"
+
+// var useralert = "hello"
+
+// alert(useralert)
+
+// var saylani_mass_it_training = "Saylani"
+// var saylaniMassItTraining = "Saylani"
+
+// alert(saylani_mass_it_training)
+// alert(saylaniMassItTraining)
+
+
+
+
+// _
+// $
+
+// var num = 2 + 6     // 8
+// var num1 = 2 - 9    // 7
+// var num2 = 3 * 4    // 12
+// var num3 =  18 / 3  // 6
+// var num4 = 54 % 3   // 0
+
+// alert(num)
+// alert(num1)
+// alert(num2)
+// alert(num3)
+// alert(num4)
+
+
+// var num = 3
+// // num = num + 1
+// num++
+// alert(num)
+
+
+// num++  -->  postincrement
+// num--  -->  postdecrement
+// ++num  -->  preincrement
+// --num  -->  predecrement
+
+
+// var num = 5
+
+// // num++   // 6
+// // num++   // 7
+// // num--   // 6
+// // ++num   // 7
+// console.log(num-- , num)  // 5, 4
+// // console.log(--num)  // 4
+// // console.log(num);   // 4
+
+
+// var a = 3
+
+// var res = a++ + a-- - --a + ++a
+//         // 3  +  4   -  2  + 3
+// console.log(res);
+
+
+// var a = 4
+// var b = 5
+// var res = --b + ++a - ++b + a++ - b--
+//     //     4  +  5   -  5  +  5  - 5
+// console.log(res);
+
+// bodmas
+// bracket
+// order
+// divide
+// multiply
+// addition
+// subtraction
+
+
+// var resultOfComputation = (2 * 4) * (4 + 2);
+
+// console.log(resultOfComputation);
+
+
+// console.log("hello" + " world");
+
+// var firstname = "Ali"
+// var lastname = "Ahmed"
+// var city = "Karachi"
+// console.log("Welcome, " + firstname + " " + lastname + " From " + city);
+
+
+
+// ========== Chapter 01 ======= //
+
+// Question 01:
+// alert("Welcome")
+
+// Question 02:
+// alert("Error, please enter a valid password" )
+
+
+// Question 05:
+// alert("Welcome to JS Land...\n Happy Coding")
+
+
+// ========== Chapter 02 ======= //
