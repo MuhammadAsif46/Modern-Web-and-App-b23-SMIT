390ca4606c082308157f12bbbd172010839d69f8
muhammadasif46 <asifahmed32c@gmail.com>
2026-07-31 12:32:36 +0500

    javascript - alert, variables string and number

---
+++ 03. JAVASCRIPT/01. class-one/app.js
@@ -0,0 +1,64 @@
+// alert("Hello world")
+
+// alert() // method
+
+
+// DataType
+
+34534645    // number
+
+true/false  // boolean
+
+"ahmed "    // string
+"645635383"
+"false"
+
+
+// alert(654393434)
+// alert("pakistan")
+
+// alert("ahmed")
+
+// var username = "Ali ahmed";
+
+// alert(username) // ali ahmed
+
+// username = "Ali Faraz"
+
+// alert(username) // ali faraz
+
+// alert(username)
+// alert(username)
+// alert(username)
+// alert(username)
+
+// var username;
+
+// username = "Jhon"
+
+// alert(username)
+
+
+// var num = 140;
+
+// // num = 200
+// var sum = num + 100
+
+// alert(sum) 
+
+
+// alert('hello')
+
+// var fullName = "Ali Raza"
+// alert(fullName)
+
+// var age = 32
+
+
+// var city = "karachi"
+
+
+
+
+
+
