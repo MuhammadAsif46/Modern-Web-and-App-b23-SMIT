20b446f87d2c54cf83c6b29f4be38ed876b9acf5
muhammadasif46 <asifahmed32c@gmail.com>
2026-08-04 17:47:26 +0500

    today class if statement and operators

---
+++ 03. JAVASCRIPT/04. chapter-09-to-11/app.js
@@ -0,0 +1,57 @@
+// ======= Chapter no : 9 -> Prompt ======== //
+
+// var username = prompt("Enter your Name?", "ali")
+// console.log(username);
+
+// var fav = +prompt("Enter your fav no?")
+// console.log(fav + 5);
+
+// console.log("10" + 5);
+
+// alert ()  // wrong syntax
+// alert()  // right syntax
+
+
+// ======= Chapter no : 10 -> If Statements ======== //
+
+    // condition check krna only true
+
+// if (true) {
+//     console.log("test");
+// }
+
+
+// ======= Chapter no : 11 -> Comparison operators ======== //
+
+
+// > greater than
+// < less than
+// >= greater than equal
+// <= less than equal
+// == equal         --> only check values
+// != not equal     --> only check values
+// === equal        --> both check values & Data type
+// !== not equal    --> only check value & Data type
+
+5 > 5  // false
+5 < 6  // true
+4 == 4  // true
+5 != 4  // true
+6 >= 6  // true
+7 <= 10  // true
+
+// if(5 == 6 ){ // true
+//     console.log("Condition is true");
+// }
+
+// var a = "hello"
+// var b = "hell0"
+
+// if(6 == "9" ){ // true
+//     console.log("Condition is true");
+// } else {
+//     console.log("Condition is false");
+// }
+
+
+
