4f30269d1bf70253850ed061282f54dc7a296df6
muhammadasif46 <asifahmed32c@gmail.com>
2026-08-04 17:47:01 +0500

    update folder names

---
+++ 03. JAVASCRIPT/03. chapter-09-and-assignment-work/app.js
@@ -0,0 +1,69 @@
+
+
+
+// ========== Chapter 01 ======= //
+
+// Question 01:
+// alert("Welcome")
+
+// Question 02:
+// alert("Error, please enter a valid password" )
+
+
+// Question 05:
+// alert("Welcome to JS Land...\n Happy Coding")
+
+
+// ========== Chapter 02 ======= //
+
+// Question 01:
+// var username;
+
+// Question 02:
+// var myName = "Muhammad Asif"
+
+
+// Question 03:
+// var message = "Hello world"
+// alert(message)
+
+
+// Question 04:
+// var username = "Jhon"
+// var age = "15 years old"
+// var course = "web-and-app-development"
+
+// alert(username)
+// alert(age)
+// alert(course)
+
+
+// Question 05:
+
+// var food = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(food)
+
+// Question 06:
+// var email = "asif@gmail.com"
+// alert("My email address is " + email)
+
+
+// Question 07:
+// var book = "Javascript"
+// alert("Learn " + book )
+
+// Question 08:
+// var content = "Yah! I can write HTML Content";
+// document.write("<h1> " + content + "</h1>")
+
+
+// Question 09:
+
+// var store = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(store)
+
+
+
+// var username = prompt("Enter Your Name?", "welcome")
+var phone = prompt("Enter Your Phone?", "+92")
+console.log(phone);
