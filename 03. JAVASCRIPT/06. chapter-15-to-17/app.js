f68550d752d9ae2df391d004552da17ade4b2e41
muhammadasif46 <asifahmed32c@gmail.com>
2026-08-12 22:09:19 +0500

    JS class chapters covered 15 to 17 arrays

---
+++ 03. JAVASCRIPT/06. chapter-15-to-17/app.js
@@ -0,0 +1,84 @@
+
+
+// var city = "Karachi";
+
+// city = "Islamabad"
+
+// console.log(city);
+
+
+// var marks = [12, 56, 78, 98]
+// var details = [34, "ali", "male", 745643653, true]
+
+// var cities = ["Karachi", "Lahore", "Pindi", "Multan"]
+// 0   ,   1    ,   2     , 3
+
+// length start 1
+// index start 0
+
+// console.log(cities);
+
+
+// cities[0] = "Kolachi"
+// cities[3] = "KPK"
+// console.log(cities[0]);
+// console.log(cities);
+// 
+
+
+// console.log(cities[2]);
+// console.log(cities[1]);
+// console.log(cities[3]);
+
+
+
+// var cities = ["Karachi", "Lahore", "hjidfw", "|jhebf", "112", 323, 23432456, 3454356, 23453245, 45234564, 3454364, 4565, "abc", true, "false"]
+
+// console.log(cities);
+
+// var cities = ["Karachi", "Lahore", "Pindi", "Multan"]
+
+// console.log(cities); // 4
+
+
+// cities[2] = "Pindi"
+// cities[3] = "KPK"
+// cities[15] = "islamabad"
+
+
+// push method: array ky last mein jakay value add krna
+// pop method: array ky last mein jakay value ko remove krna
+
+// unshift method: array ky start mein jakay value ko add krna
+// shift method: array ky start mein jakay value ko remove krna
+
+// slice method:
+// splice method: 
+
+
+// cities.push("pakistan", "Zindabad")
+// cities.pop()
+// cities.pop()
+// console.log(cities); // 2
+
+// cities.unshift("KPK", "Isl")
+// cities.shift()
+// console.log(cities); // 3
+
+
+
+// var cities = ["Karachi", "Lahore", "Pindi", "Multan"]
+// // console.log(cities); // 4
+
+
+// cities.splice(2, 1, "KPK", "isl")
+// console.log(cities);
+
+// ["karachi", "lahore", "KPK", "ISL", "MULTAN"]
+
+// var cities = ["Karachi", "Lahore", "Pindi", "Multan", "KPK", "isl", 12 ,3434,5645,656,7,567,6,86,78,67,8,8,8,67,7856,78,67]
+
+// var res = cities.slice(2, 5)
+
+// console.log(res);
+// console.log(cities.length);
