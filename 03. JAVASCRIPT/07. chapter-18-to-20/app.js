bdf3518ca179ee7abd5551621923256a6592de87
muhammadasif46 <asifahmed32c@gmail.com>
2026-08-15 18:13:10 +0500

    JS class chapters covered 18 to 20 loops

---
+++ 03. JAVASCRIPT/07. chapter-18-to-20/app.js
@@ -0,0 +1,86 @@
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+
+// for loop:
+
+// var table = +prompt("Enter fav No?")
+
+// initiliazation, condition, increment
+// for(var i = 1; i <= 8; i++){
+//     console.log( table + " x " + i + " = " + table * i);
+// }
+
+// 0 < 3 true  => 0
+// 1 < 3 true  => 1
+// 2 < 3 true  => 2
+// 3 < 3 false  => 
+
+
+// var cities = ["Khi", "Isl", "Lhr", "Pindi", "multan", "hydrabad", "peshawar"]
+
+// for(var i = 0; i < cities.length ; i++){
+//     console.log(cities[i]);
+// }
+
+
+// khi
+// Isl
+// Lhr
+
+// for (var i = 0; i < 10; i++) {
+//     console.log(i);
+//     if(i == 5){
+//         break
+//     }
+// }
+
+// var city = prompt("Enter Your City")
+
+// var cities = ["Islamabad", "Lahore", "Pindi", "Karachi"]
+// var found = false
+
+
+// for(var i = 0; i < cities.length ; i++){
+//     // console.log(cities[i]);
+
+//     // if(cities[i] == "Pindi"){
+//     //     break
+//     // }
+//     if(cities[i] == city){
+//         found = true;
+//         break
+//     }
+
+// }
+
+// if(found){
+//     console.log("Welcome to our City")
+// } else {
+//     console.log("Good Bye to our City")
+// }
+
+
+// for (var i = 0; i < 5; i++) {
+//     for (var j = 0; j < 3; j++) {
+//         console.log(i, j);
+//     }
+// }
+
+// 0, 0
+// 0, 1
+// 0, 2
+// 1, 0
+
+// var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+// var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+// var fullNames = [];
+// for (var i = 0; i < firstNames.length; i++) {
+//     for (var j = 0; j < lastNames.length; j++) {
+//         fullNames.push(firstNames[i] + lastNames[j]);
+//     }
+// }
+
+// console.log(fullNames); 
