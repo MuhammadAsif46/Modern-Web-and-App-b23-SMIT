b73897263234cf6b377289b4b1e5bd275fee86a6
muhammadasif46 <asifahmed32c@gmail.com>
2026-08-22 17:53:08 +0500

    today class JS 10 chapters covered 21 to 30

---
+++ 03. JAVASCRIPT/08. chapter-21-to-30/app.js
@@ -0,0 +1,132 @@
+// ========= chapters 21 to 30 ============ //
+
+// string methods:
+
+// toLowerCase()
+// toUpperCase()
+// slice()
+// length
+// indexOf()
+// lastIndexOf()
+// charAt()
+// replace()
+// replaceAll()
+
+
+// var username = "saylani"
+
+// console.log(username);
+// console.log(username.toUpperCase());
+
+// var school = "SMIT"
+// console.log(school);
+// console.log(school.toLowerCase());
+
+// var fullName = "saylani"
+// console.log(fullName.length);
+
+
+// var firstChar = fullName.slice(0, 1).toUpperCase()
+// var remainChar = fullName.slice(1)
+// var res = firstChar + remainChar
+// console.log(res);
+
+// console.log(firstChar);
+// console.log(remainChar);
+
+
+
+// var text = "the Second World War Word"
+
+// var find = text.indexOf("l")
+// var find = text.indexOf("W")       // 11 
+// var find1 = text.lastIndexOf("W")  // 21
+// console.log(find);
+// console.log(find1);
+
+// var findChar1 = text.charAt(11)   // W
+// var findChar2 = text.charAt(22)   // W
+// console.log(findChar1);
+// console.log(findChar2);
+
+
+// var fullname = "BSCS"
+
+// var data = console.log(fullname.slice(2))
+
+// fullname
+
+
+// var text2 = "the Second World War first string inside Second War"
+// console.log(text2);
+
+// // var rep = text2.replace("Second", "2nd")
+// var rep = text2.replaceAll("Second", "2nd")
+// console.log(rep);
+
+
+
+// number methods:
+
+// round() // = 5 > 5 ===> +1
+// ceil()
+// floor()
+// round()
+// parseInt()
+// parseFloat()
+// Number()
+// toString()
+// toFixed()
+
+// var num = 2.67
+// console.log(Math.round(num)); 
+
+
+// var num = 2.1
+// console.log(Math.ceil(num)); 
+
+// var num = 2.9
+// console.log(Math.floor(num)); 
+
+
+// var number = Math.random() * 10000000000
+// console.log(Math.floor(number));
+
+// var number = Math.random() * 6 + 1
+// console.log(Math.floor(number));
+
+
+// parseInt("123")
+// 123
+// parseInt("123.76474")
+// 123
+
+
+// +
+// parseInt
+// parseFloat
+// Number
+
+
+// Number("123")
+// 123
+// Number("123.8945393")
+// 123.8945393
+
+
+// var rollNo = 3254
+// console.log(typeof rollNo);
+// console.log(typeof rollNo.toString());
+
+
+// var nums = 567.123456
+// console.log(nums);
+
+// console.log(nums.toFixed(4));
+
+// console.log(Number(nums.toFixed(4)));
+
+
+
+// 30205060
+// 30 + 20 + 50 + 60 = 160 
