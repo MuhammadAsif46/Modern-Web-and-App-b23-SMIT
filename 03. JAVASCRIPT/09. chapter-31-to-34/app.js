9f18aca70ac8d24f183f741d9252104c5b5e4059
muhammadasif46 <asifahmed32c@gmail.com>
2026-08-27 18:03:01 +0500

    today class JS 4 chapters covered 31 to 34

---
+++ 03. JAVASCRIPT/09. chapter-31-to-34/app.js
@@ -0,0 +1,33 @@
+// ========= chapters 31 to 34 ============ //
+
+// var today = new Date();
+// console.log(today);
+
+// // var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
+// // console.log(days[today.getDay()]);
+// console.log(today.getTime());
+
+
+
+var today = new Date()
+// var firstRamadan = new Date("February 8, 2027")
+
+// var num1 = firstRamadan.getTime()
+// var num2 = today.getTime()
+
+// console.log(num1 - num2);
+
+// var res = num1 - num2;
+// var finalDate = res / (1000 * 60 * 60)
+// console.log( Math.floor(finalDate) + " days remaining");
+
+// console.log(today.getTime());
+// console.log(firstRamadan.getTime());
+// console.log(today);
+// console.log(today.setFullYear(2004));
+// console.log(today);
+
+
+
+
