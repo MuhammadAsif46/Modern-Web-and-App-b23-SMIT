ad499d2f7da86d159cd55b841f554fa69790add3
MuhammadAsif46 <asifahmed32c@gmail.com>
2026-09-01 18:03:10 +0500

    today class JS topics cover Functions chapter 35 to 38

---
+++ 03. JAVASCRIPT/11. chapter-35-to-38/app.js
@@ -0,0 +1,71 @@
+// moment().format();
+
+
+
+// console.log(moment().format());
+
+
+// var date = new Date("September 1, 2026 16:10:15")
+// // console.log(moment(date).format("MMMM ddd YYYY"));
+
+// var res = moment(date).fromNow(); 
+// console.log(res);
+
+
+// function abc(){
+//     console.log("hello");
+// }
+
+// abc()
+
+// function greet(a, b , c){ // parameters
+//     // console.log("Welcome, " + a);
+//     console.log(a , b, c);
+//     // document.write("Welcome, " + a);
+// }
+
+// // greet("Ahmed") // arguments
+// greet("Salman", 23, true) 
+
+
+// var username = prompt("Enter your name")
+// greet(username)
+
+
+// function sum(a, b) {
+//     console.log(a + b);
+// }
+
+// sum(10, 14) // 24
+// sum(6, 3) // 9
+// sum(-4, -3) // -7
+
+
+// var a = 13
+
+// console.log(a);
+
+// function foo(){
+//     var username = "faraz"
+//     return username
+
+//     console.log("hello");
+
+// }
+
+
+// var user = foo()
+// console.log(user);
+
+
+// var names = prompt("Enter your name")
+// console.log(names);
+
+
+function mul(y, z) {
+    // console.log(y , z);
+    return y * z
+}
+
+var res = mul(3, 8)
+console.log(res);
