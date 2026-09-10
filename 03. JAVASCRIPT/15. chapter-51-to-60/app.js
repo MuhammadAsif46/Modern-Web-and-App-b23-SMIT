49583dd9556e8823a0932ec8eddb2acdd3214816
MuhammadAsif46 <asifahmed32c@gmail.com>
2026-09-10 17:50:21 +0500

    today class JS topics cover chapter 51 to 60

---
+++ 03. JAVASCRIPT/15. chapter-51-to-60/app.js
@@ -0,0 +1,19 @@
+// // var heading = document.getElementById("head")
+// var heading = document.querySelector(".abc")
+
+// function setData(){
+//     // var text = "<h1>Pakistan</h1>"
+//     // heading.innerHTML = text
+//     // console.log(heading.className += " hello");
+
+//     // heading.style.backgroundColor = "red"
+//     // heading.style.color = "white"
+    
+//     console.log(heading);
+    
+    
+// }
+
+// console.log(document.childNodes[1].childNodes[2].childNodes[0].style.color = "red");
+
+
