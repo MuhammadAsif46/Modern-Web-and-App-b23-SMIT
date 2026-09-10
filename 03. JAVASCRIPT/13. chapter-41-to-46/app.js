198f6abd661c91e1c6b79384cadf208d0bc4e146
MuhammadAsif46 <asifahmed32c@gmail.com>
2026-09-10 17:49:24 +0500

    today class JS topics cover chapter 41 to 44

---
+++ 03. JAVASCRIPT/13. chapter-41-to-46/app.js
@@ -0,0 +1,17 @@
+
+// for (var i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// var i = 11;
+// while (i < 10) {
+//     console.log(i);
+//     i++
+// }
+
+var i = 6;
+do {
+    console.log(i);
+    i++
+} while (i < 5)
+
