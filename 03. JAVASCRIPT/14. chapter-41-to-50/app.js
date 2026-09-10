ae81ee25700d8133226b16498cdf77a8af0b6502
MuhammadAsif46 <asifahmed32c@gmail.com>
2026-09-10 17:49:40 +0500

    today class JS topics cover chapter 41 to 50

---
+++ 03. JAVASCRIPT/14. chapter-41-to-50/app.js
@@ -0,0 +1,37 @@
+
+
+// for(var i = 0; i < 10; i++){
+//     console.log(i);
+// }
+
+
+// var i = 0;
+// while(i < 5){
+//     console.log(i);
+//     i++
+// }
+
+// var i = 0;
+// do {
+//     console.log(i);
+//     i++;
+// } while (9 < 8)
+
+
+
+// function greet(){
+//     alert('Welcome')
+// }
+
+
+function submit (){
+    var username = document.getElementById("username")
+    console.log(username.value);
+
+    username.value = ""
+    
+}
+
+// var heading = document.getElementById("heading")
+// console.log(heading.innerHTML);
+// console.log(heading.innerText); //
