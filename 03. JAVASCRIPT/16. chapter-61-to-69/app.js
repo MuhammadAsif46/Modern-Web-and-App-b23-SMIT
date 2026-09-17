0c5c25c327edcd4c9737b709107ab020cda48ac7
muhammadasif46 <asifahmed32c@gmail.com>
2026-09-17 18:24:51 +0500

    today class JS topics cover chapter 61-70

---
+++ 03. JAVASCRIPT/16. chapter-61-to-69/app.js
@@ -0,0 +1,66 @@
+
+// var main = document.getElementById("main")
+
+// var para = document.createElement("p")
+// // para.setAttribute("id", "text")
+
+// var text = document.createTextNode("hello world")
+// // console.log(text);
+
+// para.appendChild(text)
+// main.appendChild(para)
+
+// console.log(main);
+
+
+// var main = document.getElementById("main")
+
+// main.innerHTML = `<h1>
+//             hello world
+//         </h1>
+//         <p>
+//             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam voluptas dolores iusto in, temporibus quia,
+//             accusamus, voluptatum molestiae labore ratione est omnis. Delectus recusandae dolorum odio iure quia at
+//             quas.
+//         </p>`
+
+
+
+
+// create object
+// get object data
+// update object
+// data add object
+// 
+
+// var std = {
+//     firstName: "Jhon",
+//     lastName: "Elia",
+//     age: 32,
+//     email: "jhonelia@gmail.com"
+// }
+
+// std.phone = 945203590235324053
+// std.age = 12
+
+
+
+// delete std.email
+
+// console.log(std["lastName"]);
+
+// var car = {
+//     brand: "Honda",
+//     name: "Civic",
+//     price: 3000000,
+//     model: "2035",
+//     colors: ["White", "Silver", "Black"],
+//     isAuto: true,
+//     others: {
+//         doors: 2,
+//         tyers: 5,
+//     }
+// }
+
+// // console.log(car.others.doors);
+// console.log("Honda" in car);
