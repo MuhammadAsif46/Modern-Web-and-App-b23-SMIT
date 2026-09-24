7f100a7e34e0df4ab0723af06a6968164123ba35
muhammadasif46 <asifahmed32c@gmail.com>
2026-09-24 18:13:02 +0500

    today class JS topics cover chapter 76-to-89 book end

---
+++ 03. JAVASCRIPT/18. chapter-76-to-89/app.js
@@ -0,0 +1,58 @@
+// console.log(window.location.href);
+// console.log(window.location.hostname);
+
+
+// window.location.href
+// 'https://en.wikipedia.org/wiki/Pakistan'
+// window.location.hostname
+// 'en.wikipedia.org'
+// window.location.pathname
+// '/wiki/Pakistan'
+// window.location.pathname
+// '/wiki/Pakistan'
+// window.location.hash
+// '#Geography'
+// window.location.hash
+// '#History_2'
+
+
+// function goToGoogle() {
+//     // window.location.href = "https://www.google.com"
+//     // window.location.href = "./home.html;"
+
+//     // window.location.assign("https://www.x.com");
+//     // window.location.replace("https://www.x.com");
+// }
+
+// function reload() {
+//     window.location.reload()
+// }
+// function back() {
+//     history.back()
+// }
+// function forward() {
+//     history.forward()
+// }
+
+
+// function openNewWindow() {
+//     window.open("./home.html", "win1", "width=420,height=380,left=450,top=250")
+// }
+
+
+// console.log(1);
+// aler("hello world")
+// console.log(2);
+
+console.log(1);
+
+try {
+    if (7 < 8) {
+        throw "Please enter at least 8 characters.";  
+    }
+} catch (err) {
+    console.log(err);
+}
+
+console.log(2);
+
